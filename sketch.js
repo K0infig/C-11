bde3e0339dff695a17215b521b611fc031ad8275
K0infig <71273189+K0infig@users.noreply.github.com>
2021-07-07 10:17:54 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -13,9 +13,10 @@ function setup(){
     createCanvas(400, 400);
     
     //create sprites here
-    path = createSprite(205, 195);
+    path=createSprite(200,200);
     path.addImage(pathImg);
-    path.velocity=4;
+    path.velocityY = 4;
+    path.scale=1.2;
 
     
      
@@ -33,7 +34,7 @@ function draw() {
      }
      
      if(keyIsDown(LEFT_ARROW)){
-       runner.veloctiyY = -10;
+       runner.x -= 3;
      }
      
      
